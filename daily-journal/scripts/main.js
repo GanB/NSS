4dd778929901c202fcbbd689fb31667f179b5dcf
Ganesh Babu <wayhra65@gmail.com>
2022-08-04 20:27:30 -0400

    daily journal-1 updates

---
+++ daily-journal/scripts/main.js
@@ -0,0 +1,7 @@
+console.log("hello from main.js");
+// const mainContainer = document.querySelector("#container");
+// const applicationHTML = `
+// <h1>This is where you add JavaScript</h1>
+
+// `;
+// mainContainer.innerHTML = applicationHTML;
