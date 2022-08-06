30a0112aae79655c5549cf6da2b49bbd43d063e4
“Ganesh <ganesh.b@live.com>
2022-08-05 21:09:24 -0400

    daily journal part 2 JS

--- daily-journal/scripts/main.js
+++ daily-journal/scripts/main.js
@@ -1,7 +1,72 @@
-console.log("hello from main.js");
-// const mainContainer = document.querySelector("#container");
-// const applicationHTML = `
-// <h1>This is where you add JavaScript</h1>
+const journalEntries = [
+  {
+    id: 1,
+    date: "08/01/2022",
+    concept: "HTML 5 and CSS Styling",
+    entry: "I was learning about CSS styling selectors",
+    mood: "Happy",
+  },
+  {
+    id: 2,
+    date: "08/02/2022",
+    concept: "Complex Flexbox",
+    entry:
+      "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+    mood: "Excited",
+  },
+  {
+    id: 3,
+    date: "08/03/2022",
+    concept: "CSS Grid layout",
+    entry:
+      "I practiced some CSS grid layout and HTML. It was very interesting to learn about grid layout.",
+    mood: "Happy",
+  },
+  {
+    id: 4,
+    date: "08/04/2022",
+    concept: "Javascript basics",
+    entry: "I was learning and practising javascript basics.",
+    mood: "Happy",
+  },
+  {
+    id: 5,
+    date: "08/04/2022",
+    concept: "Javascript Loops",
+    entry: "I was practising javascript basics and loops",
+    mood: "Excited",
+  },
+  {
+    id: 6,
+    date: "08/05/2022",
+    concept: "Javascript Data Structures",
+    entry: "Learning about Javascript data structures - arrays and objects",
+    mood: "Happy",
+  },
+  {
+    id: 7,
+    date: "08/05/2022",
+    concept: "Parctising Daily Journal part 2",
+    entry: "Implementing javascript in daily journal project",
+    mood: "Excited",
+  },
+];
 
-// `;
-// mainContainer.innerHTML = applicationHTML;
+console.log("-----------All Entries------------------------------");
+
+for (const entry of journalEntries) {
+  console.log(entry);
+}
+
+console.log("-----------Title------------------------------");
+
+for (const entry of journalEntries) {
+  console.log("title: " + entry.concept);
+}
+
+console.log("-----------Odd Entries------------------------------");
+for (const entry of journalEntries) {
+  if (entry.id % 2 !== 0) {
+    console.log(entry);
+  }
+}
